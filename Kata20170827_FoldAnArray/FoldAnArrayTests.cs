3e008326bf78cdfcb7ff2041a49f053928c7e990
cash <cash@cashwu.com>
2017-08-27 17:55:27 +0800

    init

--- Kata20170827_FoldAnArray/FoldAnArrayTests.cs
+++ Kata20170827_FoldAnArray/FoldAnArrayTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170827_FoldAnArray
 {
     [TestClass]
-    public class UnitTest1
+    public class FoldAnArrayTests
     {
         [TestMethod]
         public void TestMethod1()
