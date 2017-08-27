d72a3a442292bfedb85154fffd70d37fa1775c67
cash <cash@cashwu.com>
2017-08-27 18:00:09 +0800

    input array 1, 2 and runs 1 test case

--- Kata20170827_FoldAnArray/FoldAnArrayTests.cs
+++ Kata20170827_FoldAnArray/FoldAnArrayTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170827_FoldAnArray
@@ -7,8 +8,24 @@ namespace Kata20170827_FoldAnArray
     public class FoldAnArrayTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_1_2_and_1_should_return_3()
         {
+            FoldArrayShouldBe(new[] { 3 }, new[] { 1, 2 }, 1);
+        }
+
+        private static void FoldArrayShouldBe(int[] expected, int[] array, int runs)
+        {
+            var kata = new Kata();
+            var actual = kata.FoldArray(array, runs);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int[] FoldArray(int[] array, int runs)
+        {
+            return new[] { array[0] + array[1] };
         }
     }
 }
